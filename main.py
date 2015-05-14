f275ab8cd0c75d522f3ec0c44dca5eef7851cf33
Marshall Quander <marshall@quixey.com>
2015-05-14 14:35:39 -0700

    Changes to try to make it a little faster

--- main.py
+++ main.py
@@ -7,31 +7,31 @@ import random
 import logging
 import math
 
-def union(*iterables):
-    "Return a set that is the union of all the given iterables."
-    results = set()
-    for iterable in iterables:
-        results.update(iterable)
-    return results
-
 def candidates_for(board, n, dimension, x, y):
     """
     Given a point on an existing board, return the set of numbers
     which could be placed there without violating constraints.
     """
-    row_elements = board[y]
-    column_elements = (row[x] for row in board)
+    candidates = set(xrange(n))
 
-    def box_elements(board, dimension, x, y):
-        "Returns all of the numbers in the same box as (x, y)."
-        box_x, box_y = int(x / dimension), int(y / dimension)
-        for y in range(box_y * dimension, (box_y + 1) * dimension):
-            row = board[y]
-            for x in range(box_x * dimension, (box_x + 1) * dimension):
-                yield row[x]
+    # elements on the same row
+    for val in board[y]:
+        candidates.discard(val)
 
-    existing = union(row_elements, column_elements, box_elements(board, dimension, x, y))
-    return filter(lambda x: x not in existing, range(n))
+    # elements in the same column
+    for row in board:
+        candidates.discard(row[x])
+
+    # elements in the same box
+    box_x, box_y = int(x / dimension), int(y / dimension)
+    y_range = range(box_y * dimension, (box_y + 1) * dimension)
+    x_range = range(box_x * dimension, (box_x + 1) * dimension)
+    for y in y_range:
+        row = board[y]
+        for x in x_range:
+            candidates.discard(row[x])
+
+    return candidates
 
 def next_slot(n, x, y):
     """
@@ -44,17 +44,21 @@ def next_slot(n, x, y):
     else:
         return (x + 1, y)
 
-def complete_board(board, n, dimension, x, y):
+def complete_board(board, n, dimension, x, y, shuffle=True):
     """
     Produces a completion of the current board which has been filled
     left-to-right, top-to-bottom, up to but not including point (x, y).
     """
     candidates = candidates_for(board, n, dimension, x, y)
-    random.shuffle(candidates)
+    if shuffle:
+        candidates = list(candidates)
+        random.shuffle(candidates)
 
     for val in candidates:
         board[y][x] = val
         new_x, new_y = next_slot(n, x, y)
+        if new_y > y:
+            logging.info("Beginning to attempt row %s.", new_y)
         if new_y >= n: # we're done
             return board
         else:
@@ -62,14 +66,15 @@ def complete_board(board, n, dimension, x, y):
             if completion:
                 return completion
             else:
-                logging.debug("Failed to place {} in slot ({}, {}); available candidates are {}.".format(val, x, y, candidates))
                 board[y][x] = None
+                logging.debug("Failed to place %s in slot (%s, %s); available candidates are %s.", val, x, y, candidates)
+
 
     # nothing worked
-    logging.debug("Gave up on filling slot ({}, {}) with candidates {}; backtracking.".format(x, y, candidates))
+    logging.debug("Gave up on filling slot (%s, %s) with candidates %s; backtracking.", x, y, candidates)
     return None
 
-def generate_board(dimension):
+def generate_board(dimension, **kwargs):
     """
     Generates a random valid Sudoku board of the given dimension using
     backtracking. The board will be represented as a list of length
@@ -79,7 +84,7 @@ def generate_board(dimension):
     # board elements contain None when they are not yet determined
     n = dimension ** 2
     empty_board = [[None for _ in range(n)] for _ in range(n)]
-    return complete_board(empty_board, n, dimension, 0, 0)
+    return complete_board(empty_board, n, dimension, 0, 0, **kwargs)
 
 def format_board(board, dimension):
     "Returns a formatted representation of the board, one row per line."
@@ -92,10 +97,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("dimension", type=int, help="The dimension of the board (e.g. 3 makes a 9x9 board.)")
     parser.add_argument('--verbose', '-v', action='count', help="Whether to include verbose debugging output.")
+    parser.add_argument('--shuffle', action='store_true', help="Whether to generate a random board or just a valid one.")
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
-    board = generate_board(args.dimension)
+    board = generate_board(args.dimension, shuffle=args.shuffle)
     if not board:
         print "Couldn't generate a valid board."
     else:
